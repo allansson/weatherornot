dc0577a84eae9f47b3bdb857f9c6d5747c0dbc09
Johan Suleiko Allansson <johan.allansson@lifvs.com>
2019-11-21 18:29:25 +0100

    Styling is pretty close now

--- App.js
+++ App.js
@@ -3,112 +3,220 @@
  * https://github.com/facebook/react-native
  *
  * @format
- * @flow
  */
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import React, {useState, useEffect} from 'react';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {View, SafeAreaView, StyleSheet, StatusBar, Text} from 'react-native';
 
-const App: () => React$Node = () => {
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
+import {format} from 'date-fns';
+
+const CurrentTime = ({date}) => (
+  <View>
+    <SecondaryText style={styles.primary}>
+      {format(date, 'HH:mm')}
+    </SecondaryText>
+  </View>
+);
+
+const CurrentDate = ({date}) => (
+  <View>
+    <SecondaryText>{format(date, 'd MMM').toUpperCase()}</SecondaryText>
+  </View>
+);
+
+const Header = () => {
+  const [date, setDate] = useState(new Date());
+
+  useEffect(() => {
+    const handle = setTimeout(() => setDate(new Date()), 1000);
+
+    return () => clearTimeout(handle);
+  }, [date]);
+
+  return (
+    <View style={styles.header}>
+      <CurrentTime date={date} />
+      <CurrentDate date={date} />
+    </View>
+  );
+};
+
+const toIconName = state => {
+  switch (state) {
+    case 'showers':
+      return 'weather-rainy';
+
+    case 'loading':
+      return 'loading';
+
+    default:
+      return 'emoticon-sad-outline';
+  }
+};
+
+const styledText = baseStyle => ({style, ...props}) => {
+  const mergedStyle = {
+    ...baseStyle,
+    ...style,
+  };
+
+  return <Text style={mergedStyle} {...props} />;
+};
+
+const WeatherIcon = ({current}) => {
+  return <Icon style={styles.weatherIcon} name={toIconName(current)} />;
+};
+
+const Temperature = ({value}) => {
+  const text = value ? `${value}°` : '-';
+
+  return <PrimaryText>{text}</PrimaryText>;
+};
+
+const toWeatherText = kind => {
+  switch (kind) {
+    case 'sunny':
+      return 'SUNNY';
+
+    case 'showers':
+      return 'SHOWERS';
+
+    default:
+      return '-';
+  }
+};
+
+const Weather = ({temperature, kind = 'unknown'}) => {
+  return (
+    <View style={styles.weather}>
+      <WeatherIcon current={kind} />
+      <Temperature value={temperature} />
+      <SecondaryText>{toWeatherText(kind)}</SecondaryText>
+    </View>
+  );
+};
+
+const Footer = ({city = '-', country = '-'}) => {
+  return (
+    <View style={styles.footer}>
+      <SecondaryText>{city.toUpperCase()}</SecondaryText>
+      <SecondaryText>{country.toUpperCase()}</SecondaryText>
+    </View>
+  );
+};
+
+const Card = ({location}) => {
+  const [weather, setWeather] = useState();
+
+  useEffect(() => {
+    setTimeout(() => setWeather({temperature: 13, kind: 'showers'}), 1000);
+  }, [location]);
+
+  return (
+    <View style={styles.card}>
+      <Header />
+      <Weather {...weather} />
+      <Footer city={location.city} country={location.country} />
+    </View>
+  );
+};
+
+const App = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
+        <View style={styles.container}>
+          <Card
+            location={{
+              city: 'London',
+              country: 'UK',
+              lattitude: '51.507222',
+              longitude: ' -0.1275',
+            }}
+          />
+        </View>
       </SafeAreaView>
     </>
   );
 };
 
+const colors = {
+  dark: '#32353d',
+  yellow: '#e1c866',
+  primary: '#6d5c23',
+  contrast: '#fffceb',
+};
+
+const theme = {
+  primary: {
+    color: colors.primary,
+  },
+  secondary: {
+    color: colors.contrast,
+  },
+};
+
+const bar = {
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+  padding: 20,
+};
+
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  ...theme,
+  container: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: colors.dark,
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  card: {
+    backgroundColor: colors.yellow,
+    minHeight: 450,
+    minWidth: 300,
+    borderRadius: 10,
+    flexDirection: 'column',
+    justifyContent: 'space-around',
   },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
+  header: {
+    ...bar,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
   footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+    ...bar,
+    borderTopColor: colors.contrast,
+    borderTopWidth: 2,
+    paddingTop: 20,
+  },
+  primaryText: {
+    ...theme.primary,
+    fontSize: 42,
+  },
+  secondaryText: {
+    ...theme.secondary,
+    fontSize: 26,
+    fontWeight: '500',
+  },
+  weather: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    padding: 20,
+    flexGrow: 1,
+    fontSize: 72,
+  },
+  weatherIcon: {
+    fontSize: 144,
+    color: colors.contrast,
   },
 });
+
+const PrimaryText = styledText(styles.primaryText);
+const SecondaryText = styledText(styles.secondaryText);
 
 export default App;
